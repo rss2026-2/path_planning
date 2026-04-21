349d5c4f42eea2c30b973e98f26e6b9698410be7
DwnNyxDev <vatelvandell@gmail.com>
2026-04-20 21:31:43 -0400

    Added clearance safety metrics to path comparison

--- path_planning/analyze_plans.py
+++ path_planning/analyze_plans.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseArray, PoseStamped
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, OccupancyGrid
 import numpy as np
 
 import time
@@ -9,6 +9,8 @@ import math
 
 
 from path_planning.utils import LineTrajectory
+from scipy.spatial.transform import Rotation as R
+import cv2
 
 
 class PathAnalyzer(Node):
@@ -28,6 +30,7 @@ class PathAnalyzer(Node):
 
         self.goal_sub = self.create_subscription(PoseStamped, "/goal_pose", self.goal_cb, 10)
         self.pose_sub = self.create_subscription(Odometry, self.odom_topic, self.pose_cb, 10)
+        self.create_subscription(OccupancyGrid, '/map', self.map_cb, 1)
 
         for traj_topic, traj_name in zip(self.traj_topics, self.traj_names):
             self.create_subscription(
@@ -40,13 +43,62 @@ class PathAnalyzer(Node):
         self.start_time = None
         self.pose = None
 
+        self.dist_map = None  # will be populated from /map
+        self.resolution = None
+        self.origin_x = None
+        self.origin_y = None
+        self.map_yaw = None
+
         self.data = {"goal_distances": []}
         for traj_name in self.traj_names:
             self.data[traj_name] = {
                 "computation_times": [],
-                "path_distances": []
+                "path_distances": [],
+                "min_clearances": [],
+                "avg_clearances": []
             }
         self.get_logger().info("Ready to start analysis.")
+
+    def map_cb(self, msg):
+        self.resolution = msg.info.resolution
+        self.origin_x = msg.info.origin.position.x
+        self.origin_y = msg.info.origin.position.y
+        quat = [msg.info.origin.orientation.x, msg.info.origin.orientation.y,
+                msg.info.origin.orientation.z, msg.info.origin.orientation.w]
+        self.map_yaw = R.from_quat(quat).as_euler('xyz')[2]
+
+        map_data = np.array(msg.data, np.uint8)
+        binary_map = (map_data == 0).astype(np.uint8)
+        dist_pixels = cv2.distanceTransform(binary_map, cv2.DIST_L2, 5)
+        # Convert from pixels to metres
+        self.dist_map = dist_pixels * self.resolution
+        self.dist_map = self.dist_map.reshape((msg.info.height, msg.info.width))
+        self.get_logger().info('Distance map ready.')
+    
+    def world_to_grid(self, x, y):
+        tx, ty = x - self.origin_x, y - self.origin_y
+        cos_q, sin_q = np.cos(-self.map_yaw), np.sin(-self.map_yaw)
+        rx = tx * cos_q - ty * sin_q
+        ry = tx * sin_q + ty * cos_q
+        ix = int(rx / self.resolution)
+        iy = int(ry / self.resolution)
+        # Clamp to map bounds
+        ix = max(0, min(ix, self.dist_map.shape[1] - 1))
+        iy = max(0, min(iy, self.dist_map.shape[0] - 1))
+        return ix, iy
+    
+    def find_clearences(self, trajectory):
+        if self.dist_map is None:
+            self.get_logger().warn('Distance map not yet received, skipping safety metrics.')
+            return
+
+        pts = trajectory.points
+        clearances = np.array([
+            self.dist_map[self.world_to_grid(x, y)[1],
+                            self.world_to_grid(x, y)[0]]
+            for x, y in pts
+        ])
+        return (np.min(clearances), np.mean(clearances))
     
     def pose_cb(self, pose_msg):
         self.pose = {
@@ -78,10 +130,18 @@ class PathAnalyzer(Node):
         new_traj.fromPoseArray(traj_msg)
         path_dist = new_traj.distance_to_end(0)
 
+        min_clearance, avg_clearance = self.find_clearences(new_traj)
+
         self.data[traj_name]["computation_times"].append(computation_time)
         self.data[traj_name]["path_distances"].append(path_dist)
+        self.data[traj_name]["min_clearances"].append(min_clearance)
+        self.data[traj_name]["avg_clearances"].append(avg_clearance)
 
-        self.get_logger().info(f"\n{traj_name}:\n\tComputation Time: {computation_time} ms.\n\tPath Distance: {path_dist}\n\tPath Error: {path_dist - self.data['goal_distances'][-1]}")
+        self.get_logger().info(f"\n{traj_name}:\n\tComputation Time: {computation_time} ms.\n\t"+
+                               f"Path Distance: {path_dist}\n\t"+
+                               f"Path Error: {path_dist - self.data['goal_distances'][-1]}\n\t"+
+                               f"Min Clearance: {min_clearance}\n\t"+
+                               f"Avg Clearance: {avg_clearance}")
 
 def main(args=None):
     rclpy.init(args=args)
