733959c9e857bb34c124c478f6b3cbf172dae595
DwnNyxDev <vatelvandell@gmail.com>
2026-04-21 01:18:06 -0400

    changed name of file to roadmap generator to better reflect what it does

--- setup.py
+++ setup.py
@@ -35,17 +35,13 @@ setup(
         'console_scripts': [
             'trajectory_builder = path_planning.trajectory_builder:main',
             'trajectory_loader = path_planning.trajectory_loader:main',
-<<<<<<< HEAD
-=======
-            'trajectory_generator = path_planning.trajectory_generator:main',
-            'trajectory_planner = path_planning.trajectory_planner:main',
->>>>>>> 7bae3d34fac47e0132ce378dd6d28cd0b5b5d5af
             'trajectory_follower = path_planning.trajectory_follower:main',
             'prm_viz = path_planning.PRM_visualizer:main',
             'analyze_prm = path_planning.analyze_prm:main',
             'grid_search_planner = path_planning.grid_search_planner:main',
             'sampling_planner = path_planning.sampling_planner:main',
-            'path_analyzer = path_planning.analyze_plans:main'
+            'path_analyzer = path_planning.analyze_plans:main',
+            'roadmap_generator = path_planning.roadmap_generator:main'
         ],
     },
 )
